37c0b13f03f54dbb96794be4a93a0aa82509d481
Ian Edwards <ian.edward@uconn.edu>
2020-08-29 16:25:25 -0400

    First Commit
    Installed NPM Bootstrap
    Modified App.js to Application Instructions

---
+++ to-do-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+
+function App() {
+ return (
+ <div className= "container p-3 bg-primary text-center text-white">
+ <h2> To Do App </h2>
+ </div>
+ );
+}
+
+export default App;
